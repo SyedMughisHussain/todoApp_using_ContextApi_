021d7be09feed589994401a9c1651b4967fe6d48
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-01-25 22:28:47 +0500

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />}/>
-        <Route path="/signIn" element={<div>Login Page</div>}/>
-        <Route path="/signUp" element={<div>Sign Up Page</div>}/>
+        <Route path="/signIn" element={<Login />}/>
+        <Route path="/signUp" element={<SignUp />}/>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-        <nav>
-            <ul>
-                <Link to={"/signIn"}>SignIn Page</Link>
-                <Link to={"/signUp"}>SignUp Page</Link>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/signIn"}>SignIn Page</Link>
+          <Link to={"/signUp"}>SignUp Page</Link>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
